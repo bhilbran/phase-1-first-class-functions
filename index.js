66de785b57408a944db33e2b6cfa2ff46e62b41d
brooke <bhilbran@asu.edu>
2023-08-29 14:38:13 -0700

    done

---
+++ index.js
@@ -0,0 +1,20 @@
+function receivesAFunction(callback) {
+    // Call the callback function
+    callback();
+  }
+
+    function returnsANamedFunction() {
+      return function toNotDeeplyEqual(){
+
+      }
+    };
+
+    function returnsAnAnonymousFunction() {
+        return function(){
+  
+        }
+      };
+
+
+
+  
